ffb2184e5d842cd14aa599ece2067b394ad13e92
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2022-12-02 17:32:00 +0900

    seminar6

--- app/src/main/java/org/sopt/sample/SignInActivity.kt
+++ app/src/main/java/org/sopt/sample/SignInActivity.kt
@@ -2,39 +2,58 @@ package org.sopt.sample
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.sopt.sample.databinding.ActivitySignInBinding
-import org.sopt.sample.remote.RequestLoginDTO
-import org.sopt.sample.remote.ResponseLoginDTO
-import org.sopt.sample.remote.ServicePool
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 // 로그인 페이지
 class SignInActivity : AppCompatActivity() { // 여기서 class는 한 화면이라고 생각하면 된다.
     private lateinit var binding: ActivitySignInBinding
-    private var id: String? = null
-    private var password: String? = null
-    private var name: String? = null
-    private val authService = ServicePool.AuthService
+    private var validEmail: Boolean = false
+    private var validPw: Boolean = false
+    private val viewModel by viewModels<SignInViewModel>()
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root) // binding 부분 순서 주의하기
+
+
+        binding.btnSignup.setOnClickListener() {
+                startActivity(Intent(this, SignUpActivity::class.java)
+        }
+        binding.btnLogin.setOnClickListener {
+            viewModel.login(
+                binding.etSigninemail.text.toString(),
+                binding.etSigninpassword.text.toString()
+            )
+        }
+
+        viewModel.successLogin.observe(this){ success ->
+            if(success) {
+                startActivity(Intent(this, HomeActivity::class.java))
+                val homeIntent = Intent(this@SignInActivity, HomeActivity::class.java)
+                homeIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                Toast.makeText(this@SignInActivity, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                startActivity(homeIntent)
+            }
+        }
+
+        viewModel.errorMessage.observe(this){ it ->
+            Snackbar.make(binding.root, it, Snackbar.LENGTH_SHORT).show()
+        }
+
         activityResultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == RESULT_OK) {
                     Snackbar.make(binding.root, "로그인을 성공했습니다", Snackbar.LENGTH_SHORT).show()
-                   // id = result.data?.getStringExtra("userid") ?: ""
-                   // password = result.data?.getStringExtra("userpassword") ?: ""
-                   // name = result.data?.getStringExtra("username") ?: ""
                 }
             }
         clickloginbtn()
@@ -47,45 +66,15 @@ class SignInActivity : AppCompatActivity() { // 여기서 class는 한 화면이
     private fun clickloginbtn() { // 함수같은 것도 다 class에 집어 넣기
         val homeIntent = Intent(this, HomeActivity::class.java)
         binding.btnLogin.setOnClickListener() {
-            if(binding.etSigninemail.text.isEmpty()){
-                Toast.makeText(this@SignInActivity,"이메일을 입력하지 않았습니다",Toast.LENGTH_SHORT).show()
+            if (binding.etSigninemail.text.isEmpty()) {
+                Toast.makeText(this@SignInActivity, "이메일을 입력하지 않았습니다", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if(binding.etSigninpassword.text.length !in 6..10){
-                Toast.makeText(this@SignInActivity,"비밀번호가 조건에 맞지 않습니다",Toast.LENGTH_SHORT).show()
+            if (binding.etSigninpassword.text.length !in 6..10) {
+                Toast.makeText(this@SignInActivity, "비밀번호가 조건에 맞지 않습니다", Toast.LENGTH_SHORT).show()
             }
-            // 로그인 api 연결
-            authService.login(
-                RequestLoginDTO(
-                    binding.etSigninemail.text.toString(),
-                    binding.etSigninpassword.text.toString()
-                )
-            ).enqueue(object : Callback<ResponseLoginDTO> {
-                override fun onResponse(
-                    call: Call<ResponseLoginDTO>,
-                    response: Response<ResponseLoginDTO>
-                ) {
-                    // 로그인 성공했을 시
-                    if (response.isSuccessful) {
-                        Toast.makeText(this@SignInActivity, "로그인에 성공했습니다", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this@SignInActivity,HomeActivity::class.java).apply {
-                            putExtra("id", response.body()?.result?.id)
-                        }
-                        startActivity(homeIntent)
-                        finish()
-                    }
-                    else if (response.code() == 404) {
-                        Snackbar.make(binding.root, "404 error 발생", Snackbar.LENGTH_LONG).show()
-                    }
-                    else if (response.code() == 401) {
-                        Snackbar.make(binding.root, "401 error 발생", Snackbar.LENGTH_LONG).show()
-                    }
-                }
 
-                override fun onFailure(call: Call<ResponseLoginDTO>, t: Throwable) {
-                    Toast.makeText(this@SignInActivity, "서버통신에 오류가 발생하였습니다", Toast.LENGTH_SHORT).show()
-                }
-            })
+            // login 응답 값 observe(여기는 ui이기에 서버통신에서 변화되는 걸 바라보고 보여줘야 한다)
         }
     }
 
