d532aabe8bd28ca8d9c73cb89cf901be85c776b8
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2022-12-23 13:31:55 +0900

    [Refactor]: 리사이클러뷰 관련 코드 전면 수정

--- app/src/main/java/org/sopt/sample/entity/UserData.kt
+++ app/src/main/java/org/sopt/sample/entity/UserData.kt
@@ -3,10 +3,10 @@ import kotlinx.serialization.SerialName
 
 data class UserData(
     @SerialName("avatar")
-    val image: String,
+    val image: Int,
     @SerialName("first-name")
-    val name:String,
-    val author:String,
+    val name: String,
+    val author: String,
     @SerialName("email")
-    val email:String
+    val email: String
 )
--- app/src/main/java/org/sopt/sample/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/presentation/fragment/HomeFragment.kt
@@ -1,100 +1,64 @@
-package org.sopt.sample.presentation.fragment
-
+package org.sopt.sample
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import com.google.android.material.snackbar.Snackbar
-import org.sopt.sample.entity.UserData
 import org.sopt.sample.databinding.FragmentHomeBinding
-import org.sopt.sample.presentation.adapter.FollowerAdapter
-import org.sopt.sample.presentation.adapter.HomeAdapter
-import org.sopt.sample.presentation.viewmodel.FollowerViewModel
-import org.sopt.sample.data.*
-import org.sopt.sample.data.model.response.ResponseFollowerDTO
-import org.sopt.sample.data.service.FollowerService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import org.sopt.sample.entity.UserData
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = _binding!!  //java
-    private lateinit var followerAdapter: FollowerAdapter  //HomeAdapter을 homeAdpater라고 부른다.
-    private val followerViewModel by viewModels<FollowerViewModel>()
-
+    private lateinit var homeAdapter: HomeAdapter //HomeAdapter을 homeAdpater라고 부른다.
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        followerViewModel.getData()
-        followerViewModel.successGet.observe(viewLifecycleOwner){ success ->
-            if(success){   // 통신에 성공했다면 adapter를 갱신시켜라
-                val adapter = context?.let { it1 ->
-                    followerViewModel.getResult.value?.let {
-                        followerAdapter.userList()
-                    }
-                        followerAdapter(FollowerViewModel.getResult.value!!.data, it1).apply {
-                            UserData(FollowerViewModel.getResult.value!!.data) //이 부분 모르겠음음                        }
-                        }
-                    }
-                }
-                binding.rvRepos.adapter = adapter
-            }
-
-    override fun onCreatedView(view: View,savedInstanceState: Bundle?){
-        super.onViewCreated(view, savedInstanceState)
-        FollowerService.getData().enqueue(object:Callback<ResponseFollowerDTO>{
-            override fun onResponse(
-                call: Call<ResponseFollowerDTO>,
-                response: Response<ResponseFollowerDTO>
-            ) {
-                if (response.isSuccessful) {
-                   FollowerViewModel.followerList.addAll(response.body()?.data!!)
-                    val adapter = FollowerAdapter(requireContext())
-                    binding.rvHome.adapter = adapter
-                    adapter.UserList(FollowerViewModel.followerList)
-                } else if (response.code() == 404) {
-                    Snackbar.make(binding.root, "404 error", Snackbar.LENGTH_LONG)
-                        .show()
-                } else if (response.code() == 401) {
-                    Snackbar.make(binding.root, "401 error", Snackbar.LENGTH_LONG)
-                        .show()
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseFollowerDTO>, t: Throwable) {
-                Toast.makeText(this@HomeFragment,"실패",Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-
-
-    fun viewFirst() {
-        binding.rvHome.post {
-            binding.rvHome.scrollToPosition(0)
+        private fun initAdapter() {
+            homeAdapter = HomeAdapter() //HomeAdapter 내용을 homeAdapter에 저장한다.
+            binding.rvHome.adapter = homeAdapter  //홈 리사이클러 명이 rvHome인 것이다.
+            initUserData()
         }
-    } // 리사이클러 뷰의 최상단으로 이동
+        //외부에서 어댑터 내부의 리스트에 접근하면 크래쉬가 났을 때 클래스 내부만 봐도 될 문제를 다른 파일에 있는 코드도
+        // 다 뜯어봐야하는 문제가 발생할 수 있어서 최대한 자원은 내부에서 사용하도록 하고 함수를 뚫어서 클래스 내부에서 로직을 실행하도록 하는게 좋다.
+        private fun initUserData(){
+            homeAdapter.userList.addAll(
+                listOf(  //데이터 조작을 해준다. item_home은 그냥 전체적인 틀만 잡아줄 뿐이다. 그건 이제 빈 깡통.
+                    UserData(R.drawable.git,"안드1","영주","0@gmail.com"),  //데이터를 여기서 바꾸면 된다.
+                    UserData(R.drawable.git,"안드2","대환","1@gmail.com"),
+                    UserData(R.drawable.git,"안드3","하정","2@gmail.com"),
+                    UserData(R.drawable.git,"안드4","지은","3@gmail.com"),
+                    UserData(R.drawable.git,"안드5","a","4@gmail.com"),
+                    UserData(R.drawable.git,"안드6","b","5@gmail.com"),
+                    UserData(R.drawable.git,"안드7","c","6@gmail.com"),
+                    UserData(R.drawable.git,"안드8","d","7@gmail.com"),
+                    UserData(R.drawable.git,"안드9","e","8@gmail.com"),
+                    UserData(R.drawable.git,"안드10","f","9@gmail.com")
+                )
+            )
+            homeAdapter.notifyDataSetChanged()  //리스트가 업데이트가 됐다는 것을 알려준다.
+        }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //initAdapter()  //함수 생성
-        _binding=null
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        fun viewFirst() {
+            binding.rvHome.post {
+                binding.rvHome.scrollToPosition(0)
+            }
+        } // 리사이클러 뷰의 최상단으로 이동
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            initAdapter()  //함수 생성
+        }
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
     }
-}
